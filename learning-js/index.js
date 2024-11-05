5ca1a09793655b7224ae5f511a02e121d223f808
example <example.com>
2024-11-05 11:05:53 +0800

    add css and js

---
+++ learning-js/index.js
@@ -0,0 +1,42 @@
+function forkEventHandler(body) {
+  const {
+    repository: {
+      name: repo,
+      html_url: repoUrl,
+    },
+    sender: {
+      login: sender,
+      html_url: senderUrl
+    },
+  } = body
+
+  const messages = [
+    `The repo [${repo}](${repoUrl}) has been forked by [${sender}](${senderUrl})!`,
+    'Your project is spreading!',
+    `*${repo}* is reaching more people! 🌍`
+  ];
+  return messages;
+}
+
+function render() {
+  const body = {
+    repository: {
+      name: "oneimg",
+      html_url: "https://byodian/oneimg"
+    },
+    sender: {
+     login: "byodian",
+     html_url: "https://byodian/oneimg"
+    }
+  }
+
+  const app = document.getElementById("app");
+  const messages = forkEventHandler(body)
+  for (const message of messages) {
+    const p = document.createElement('p')
+    p.textContent = message
+    app.appendChild(p)
+  }
+}
+
+render()
